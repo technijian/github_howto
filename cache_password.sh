b285f9c258c04a917a737eeeac08e307cfd1901f
cebutech <mbelz@cebutech.com>
2013-10-25 11:03:39 -0700

    update

---
+++ cache_password.sh
@@ -0,0 +1,10 @@
+#replace with your github email and username
+
+
+
+git config --global user.name "cebutech"
+git config --global user.email "mbelz@cebutech.com"
+##Password caching
+git config --global credential.helper cache
+#
+git config --global credential.helper 'cache --timeout=3600'
